53203ec127ddc4fa7bf447c91b657ef0acb074a5
TrueShadowGuard <playvarian@gmail.com>
2022-12-10 14:29:21 +0300

    added files text

--- client/src/components/special/AboutPage/PersonalInfo/PersonalInfo.js
+++ client/src/components/special/AboutPage/PersonalInfo/PersonalInfo.js
@@ -2,12 +2,16 @@ import React from "react";
 import classes from "./PersonalInfo.module.css";
 import Folder from "./Folder"
 import aboutPageStore from "../aboutPageStore"
+import File from "./File";
 
-const PersonalInfo = ({ files }) => {
+const PersonalInfo = () => {
     return (
         <div className={classes.root}>
             {aboutPageStore.folders.map((folder, index) => (
-              <Folder key={index} folder={folder}/>
+              <Folder key={index + "folder"} folder={folder}/>
+            ))}
+            {aboutPageStore.files.map((file, index) => (
+              <File key={index + "file"} file={file} />
             ))}
         </div>
     );
--- client/src/components/special/AboutPage/aboutPageStore.js
+++ client/src/components/special/AboutPage/aboutPageStore.js
@@ -2,37 +2,38 @@ import { makeAutoObservable } from "mobx"
 
 class AboutPageStore {
   constructor () {
-    makeAutoObservable(this)
+    makeAutoObservable(this);
+    this.currentFile = files.find(file => file.name === "index");
   }
 
   currentFile = null
 
   folders = folders
+  files = files;
 
   setCurrentFile (currentFile) {
-    this.currentFile = currentFile
+    this.currentFile = currentFile;
   }
 }
 
 const workExperience = [{
   name: "YumaSoft",
-  important: true,
-  text: `/**
- * From June 2022 to now
- * Developed legacy auto repair shop management software
- * Fixed bugs and developed new functionality
- * Core technologies: Backbone.js, React.js
- */`,
-  minText: "From June 2022 to now. Developed legacy auto repair shop management software. Fixed bugs and developed new functionality. Core technologies: Backbone.js, React.js"
+  text: `From June 2022 to now
+Developed legacy auto repair shop management software
+Fixed bugs and developed new functionality
+Core technologies: Backbone.js, React.js`,
 }]
 
 const interests = [
   {
     name: "Dota 2",
-    important: true
+    text: "Sometimes I let myself play Dota with my friends"
   },
   {
-    name: "Math"
+    name: "Chemistry",
+    text: "I love chemistry and sometimes I study it.\n" +
+      "In the past, successfully participated in the Olympiads on it.\n" +
+      "Took a prize-winning place in the Olympiad of the state level"
   }
 ];
 
@@ -42,10 +43,10 @@ const education = [
   },
   {
     name: "Gomel State Lyceum",
+    text: `Like `
   },
   {
     name: "BSMU",
-    important: true
   }
 ]
 
@@ -60,7 +61,17 @@ const folders = [{
   name: "education",
   files: education,
   color: "#3A49A4"
-}]
+}];
+
+const files = [{
+  name: "index",
+  text: `Having a high level of javascript proficiency, I can easily solve the tasks.
+I like to help colleagues at work, as well as automate my daily activities.
+Russian level - native, English level - Intermediate
+
+Core technologies: HTML, CSS, JavaScript, react, node, mobx, redux
+Also familiar with: SQL, docker, mongo, ruby`
+}];
 
 const aboutPageStore = new AboutPageStore()
 window._aboutPageStore = aboutPageStore
